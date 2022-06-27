c5d571cd15e11a5b2cc2ed0eb4e56fa265cbf44c
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-06-27 23:58:39 +0300

    feat: init typescript in webpack

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ab358adccc454555a15ec1d6657e60e9', // получите свой ключ https://newsapi.org/
         });
     }
 }
